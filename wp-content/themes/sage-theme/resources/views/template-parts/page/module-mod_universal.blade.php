ee323369020f96bc12ca8bd1dc9443de2b090b1d
MangBao <mangbao1301@gmail.com>
2022-01-21 09:21:17 +0700

    update font

--- wp-content/themes/sage-theme/resources/views/template-parts/page/module-mod_universal.blade.php
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-mod_universal.blade.php
@@ -1,42 +1,83 @@
-<div class="mod-universal">
+<section class="mod-universal">
     <div class="container">
-        <h1>Buttons/CTAs</h1>
-        <div class="row">
-            <div class="col w-3/12">
-                <h3>
-                    Primary Button
-                </h3>
-                <p class="text-left">
-                    <a href="" class="btn btn-primary w-138 md:w-110 xl:mr-64 xl:ml-14">Book Now</a>
-                </p>
-            </div>
-            <div class="col w-3/12">
-                <h3>
-                    Secondary Button
-                </h3>
-                <p class="text-left">
-                    <a href="" class="btn btn-second w-138 md:w-110 xl:mr-64 xl:ml-14">Book Now</a>
-                </p>
-            </div>
-            <div class="col w-3/12">
-                <h3>
-                    Tertiary/Text CTA
-                </h3>
-                <p class="text-left">
-                    <a href="" class="btn btn-cta w-138 md:w-110 xl:mr-64 xl:ml-14">OUR SERVICES</a>
-                </p>
-            </div>
-            <div class="col w-3/12">
-                <h3>
-                    Social
-                </h3>
-                <p class="text-left">
-                    <a href="" class="btn btn-primary btn-social w-138 md:w-110 xl:mr-64 xl:ml-14">
-                        <span class="icomoon icon-facebook1 text-3xl mr-5 align-middle"></span>
-                        FOLLOW US
+        <div class="mod-1">
+            <h2>Buttons/CTAs</h2>
+            <div class="row">
+                <div class="col w-3/12">
+                    <h3>
+                        Primary Button
+                    </h3>
+                    <p class="text-left">
+                        <a href="" class="btn btn-primary w-138 md:w-110 xl:mr-64 xl:ml-14">Book Now</a>
+                    </p>
+                </div>
+                <div class="col w-3/12">
+                    <h3>
+                        Secondary Button
+                    </h3>
+                    <p class="text-left">
+                        <a href="" class="btn btn-second w-138 md:w-110 xl:mr-64 xl:ml-14">Book Now</a>
+                    </p>
+                </div>
+                <div class="col w-3/12">
+                    <h3>
+                        Tertiary/Text CTA
+                    </h3>
+                    <p class="text-left">
+                        <a href="" class="btn btn-cta w-138 md:w-110 xl:mr-64 xl:ml-14">OUR SERVICES</a>
+                    </p>
+                </div>
+                <div class="col w-3/12">
+                    <h3>
+                        Social
+                    </h3>
+                    <p class="text-left">
+                        <a href="" class="btn btn-primary btn-social w-138 md:w-110 xl:mr-64 xl:ml-14">
+                            <span class="icomoon icon-facebook1 text-3xl mr-5 align-middle"></span>
+                            FOLLOW US
+                        </a>
+                    </p>
+                    <a href="" class="mr-7 text-general hover:text-primary social-item">
+                        <span class="icomoon icon-facebook1 text-3xl"></span>
                     </a>
-                </p>
+                    <a href="" class="mr-7 text-general hover:text-primary social-item">
+                        <span class="icomoon icon-instagram1 text-3xl"></span>
+                    </a>
+                </div>
             </div>
         </div>
+        <div class="mod-2">
+            <h2>Typography</h2>
+            <p>
+                <h1>H1 : Large Hero Text </h1><span>(Poppins Bold, 52pt / 60pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <h2>H2 : Section Header</h2><span>(Poppins Bold, 36pt / 40pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <h3>H3 : Secondary Section Header</h3><span>(Poppins Bold, 36pt / 40pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <h4>H4 : Intro text</h4><span>(Poppins Regular, 26pt / 42pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <h5>H5 : Large Descriptors/Labels</h5><span>(Poppins Bold, 22pt / 30pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <h6>H6 : Medium Descriptors/Labels</h6><span>(Poppins Bold, 18pt / 24pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <div class="h7">H7 : Small Descriptors/Labels</div><span>(Poppins Bold, 16pt / 24pt leading / Hex: #1C575A)</span>
+            </p>
+            <p>
+                <div class="p1">P1: Large Body Copy</div><span>(Poppins Regular, 22pt / 30pt leading / Hex: #666666)</span>
+            </p>
+            <p>
+                <div class="p2">P2: Regular Body Copy</div><span>(Poppins Regular, 18pt / 24pt leading / Hex: #666666)</span>
+            </p>
+            <p>
+                <div class="p3">P3: Small Body Copy</div><span>(Poppins Regular, 16pt / 22pt leading / Hex: #666666)</span>
+            </p>
+        </div>
     </div>
-</div>
+</section>
