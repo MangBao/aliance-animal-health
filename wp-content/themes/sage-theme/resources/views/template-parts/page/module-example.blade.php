eedcd67b4abb05f8b22bf3ce81f3f504a8421bf3
hathanh <hathanh.0113@gmail.com>
2022-01-21 10:39:48 +0700

    branch develop

--- wp-content/themes/sage-theme/resources/views/template-parts/page/module-example.blade.php
+++ wp-content/themes/sage-theme/resources/views/template-parts/page/module-example.blade.php
@@ -1,13 +1,118 @@
-<div class="container">
-<h3>Module: Example {{$data->ex1}}</h3>
-    @foreach ($data->example as $item)
-        {{$item->post_title}}
-        {!! $item->post_content !!}
-    @endforeach
-
-    {{ Page::filterExample() }}
-    {{ isset($data->logo['url']) ? $data->logo['url'] : ''}}
-
-    {{-- {{print_r($data)}} --}}
-    @debug
-</div>
+<section class="module mod-banner-home bg-[#efefef]">
+  <div class="container">
+    <div class="row">
+      <div class="col w-1/2 relative min-h-[680px]">
+        <div class="w-full absolute inset-0 bg-center bg-cover" style="background-image: url('https://westvillaanimalhospital-staging.box.carbon8test.com/wp-content/uploads/sites/6/2021/10/img-banner.jpg')">
+        </div>
+      </div>
+      <div class="col w-1/2 text-center flex items-center justify-center">
+        <div class="w-full max-w-[400px]">
+          <h1>Lorem ipsum dolor sit amet, consetetur</h1>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
+          <a href="#" class="btn btn-brown">Book now</a>
+          <p class="uppercase">Or text us at (501) 888-8888</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative">
+    <div class="row">
+      <div class="col w-1/2 flex items-center justify-center">
+        <div class="w-full">
+          <h1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</h1>
+        </div>
+      </div>
+      <div class="col w-1/2 relative min-h-[430px]">
+        <div class="w-full absolute inset-0 bg-center bg-cover" style="background-image: url('@asset('images/banner/banner-our-practice-768x510.jpeg')')">
+        </div>
+      </div>
+    </div>
+    <img src="@asset('images/banner-internal-bottom-img.svg')" alt="img" class="absolute bottom-[-125px] left-[121px]">
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative">
+    <div class="row">
+      <div class="col w-1/2 relative min-h-[430px]">
+        <div class="w-full absolute inset-0 bg-center bg-cover" style="background-image: url('@asset('images/banner/tow-doctor-dog-1536x1025.jpeg')')">
+        </div>
+      </div>
+      <div class="col w-1/2 flex items-center justify-center">
+        <div class="w-full">
+          <h1>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</h1>
+        </div>
+      </div>
+    </div>
+    <img src="@asset('images/banner-internal-bottom-img.svg')" alt="img" class="absolute bottom-[-125px] right-[121px]">
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative">
+    <div class="row">
+      <div class="col w-1/2 flex items-center justify-center">
+        <div class="w-full">
+          <h1>Lorem ipsum dolor sit amet, consetetur</h1>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+          <a href="#" class="btn btn-brown">Button</a>
+        </div>
+      </div>
+      <div class="col w-1/2 relative min-h-[509px]">
+        <div class="w-full absolute inset-0 bg-center bg-cover" style="background-image: url('@asset('images/banner/Two-people-and-dog-1024x911.jpg')')">
+        </div>
+      </div>
+    </div>
+    <img src="@asset('images/banner-internal-bottom-img.svg')" alt="img" class="absolute bottom-[-125px]">
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative min-h-[354px]">
+    <div class="banner-image absolute h-full w-full inset-0 bg-center bg-cover" style="background-image: url('@asset('images/banner/contac-banner.jpg')')">
+    </div>
+    <div class="absolute transform-center-middle max-w-[600px] text-center">
+      <h1 class="text-white">Lorem ipsum dolor sit amet, consetetur</h1>
+    </div>
+    <div class="absolute bg-repeat-x bottom-[-70px] right-0 left-0 h-50" style="background-image: url('@asset('images/dot-x.svg')')">
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative min-h-[345px]">
+    <div class="absolute transform-center-middle max-w-[600px] text-center">
+      <h1>Lorem ipsum dolor sit amet, consetetur</h1>
+      <img src="@asset('images/dog-and-cat.svg')" alt="Dog Cat" class="w-25 block mx-auto">
+    </div>
+    <div class="absolute bg-repeat-x bottom-[-75px] right-0 left-0 h-50" style="background-image: url('@asset('images/dot-x.svg')')">
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative min-h-[345px]">
+    <div class="absolute transform-center-middle row justify-center items-center w-2/3">
+      <div class="col w-1/3">
+        <img src="@asset('images/icon.svg')" alt="Dog Cat" class="w-88 h-88 block mx-auto">
+      </div>
+      <div class="col w-2/3 max-w-[660px]">
+        <h1>Lorem ipsum dolor sit</h1>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+      </div>
+    </div>
+    <div class="absolute bg-repeat-x bottom-[-75px] right-0 left-0 h-50" style="background-image: url('@asset('images/dot-x.svg')')">
+  </div>
+</section>
+<div class="h-100"></div>
+<section class="module mod-banner-home bg-[#e0f6f8]">
+  <div class="container relative min-h-[231px]">
+    <div class="absolute transform-center-middle row justify-center items-center w-2/3">
+      <div class="col w-2/3 max-w-[660px]">
+        <h1>Lorem ipsum dolor sit</h1>
+      </div>
+    </div>
+    <div class="absolute bg-repeat-x bottom-[-75px] right-0 left-0 h-50" style="background-image: url('@asset('images/dot-x.svg')')">
+  </div>
+</section>
